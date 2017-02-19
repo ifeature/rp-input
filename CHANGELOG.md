4aa5d9d77b0c14003fd58a45cf0d7db8cad84829
ifeature <ifeature@yandex.ru>
2017-02-19 21:45:39 +0200

    Sourcemaps added

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,13 @@
-## 0.1.0 (May 19, 2017)
+## 0.1.2 (February 19, 2017)
+
+* Sourcemaps added
+
+
+## 0.1.1 (February 19, 2017)
+
+* Little fixes
+
+
+## 0.1.0 (February 19, 2017)
 
 * Initial release
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,40 @@
 'use strict';
 
 const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = {
     context: path.resolve(__dirname, './src'),
+    devtool: NODE_ENV ? 'cheap-inline-source-map' : 'source-map',
     entry: {
         index: './index'
     },
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        library: 'RpInput',
+        libraryTarget: 'umd'
     },
     resolve: {
         modules: ['node_modules'],
         extensions: ['.js', '.jsx']
     },
     module: {
+        noParse: /lodash/,
         rules: [
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
-                loader: 'babel-loader'
+                use: ['babel-loader']
             }
         ]
     },
+    plugins: [
+        new UglifyJSPlugin({
+            sourceMap: true
+        })
+    ],
     stats: {
         colors: true,
         modules: true,
